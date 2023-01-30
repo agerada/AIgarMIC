57590260b92b7d4bc3d3d7522b732c1a1f77d80a
agerada <sandrogerada@gmail.com>
2023-01-30 16:49:20 +0000

    Updated main

--- main.py
+++ main.py
@@ -14,16 +14,47 @@ import numpy as np
 from process_plate_image import *
 import os
 from plate import Plate
+import tensorflow as tf
+import argparse
+import random
+from utils import convertCV2toKeras
 
 def main(): 
+    parser = argparse.ArgumentParser(description="Main script to interpret agar dilution MICs")
+    parser.add_argument("-m", "--model", type=str, help="Specify folder containing tensorflow model for image classificaion")
+    args = parser.parse_args()
+
     folder = "images/gent"
     gent_images = os.listdir(folder)
+    gent_images = [i for i in gent_images if i.count('.jpg') > 0]
     plates = []
     for path in gent_images: 
         try: 
-            plates.append(Plate("gentamicin", 0, os.path.join(folder, path)))
-        except: 
-            print(path, "failed")
+            conc = float(os.path.splitext(path)[0])
+            plates.append(Plate("gentamicin", conc, os.path.join(folder, path)))
+        except Exception as e: 
+            print(f"Error while trying to import {path}: ")
+            print(e)
+    plates.sort(reverse=True)
+
+    if args.model: 
+        model = tf.keras.models.load_model(args.model)
+
+        random_images = []
+        predictions = []
+        scores = []
+        for i in range(10): 
+            random_image, _ = random.choice(plates).get_random_colony_image()
+            random_images.append(random_image)
+            prediction = model.predict(convertCV2toKeras(random_image))
+            predictions.append(prediction)
+            score = tf.nn.softmax(prediction)
+            scores.append(score)
+        class_names = ['No growth','Poor growth','Good growth']
+        for i,p,s in zip(random_images, predictions, scores): 
+            print(f"This image was predicted as: {class_names[np.argmax(s)]} with a prediction of {100 * np.max(s)}")
+            cv2.imshow('image', i)
+            cv2.waitKey()
 
 if __name__ == "__main__": 
     main()
