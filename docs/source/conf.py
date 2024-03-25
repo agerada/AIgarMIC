7d6d0422f0ca9c65d107f2bd65fcbac57d7c4b76
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2024-03-25 16:58:07 +0000

    docs requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_nb",
+    # "myst_nb",
     # "autoapi.extension",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
