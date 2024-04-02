b1bb1c0ee5f57522e385ca7197ccf23a8d0e3722
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2024-04-02 12:39:26 +0100

    argparse dep

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src', 'aigarmic')))
 
 project = 'AIgarMIC'
 copyright = '2024, Alessandro Gerada'
